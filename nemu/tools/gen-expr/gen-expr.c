25d01a41c5441bc3f1e550af8d31600a5be3eafe
tracer-ics2022 <tracer@njuics.org>
2025-02-17 22:30:48 +0800

    >  compile NEMU 211221234 李四 Linux nju-PA 6.8.0-50-generic #51~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 21 12:03:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:30:48 up 3 days, 12:52,  6 users,  load average: 0.17, 0.25, 0.26

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -19,10 +19,13 @@
 #include <time.h>
 #include <assert.h>
 #include <string.h>
+#include <sys/wait.h>  // for examine child thread exit status
+#include <signal.h>    // for SIGFPE
 
 // this should be enough
-static char buf[65536] = {};
-static char code_buf[65536 + 128] = {}; // a little larger than `buf`
+static int len = 0;
+static char buf[65536] = {0};
+static char code_buf[65536 + 128] = {0}; // a little larger than `buf`
 static char *code_format =
 "#include <stdio.h>\n"
 "int main() { "
@@ -31,20 +34,65 @@ static char *code_format =
 "  return 0; "
 "}";
 
+/* Generate random number ranges from 0 to n-1 */
+static int choose(int n) {
+  return rand() % n;
+}
+
+static void gen_num() {
+  /* The random number ranges from 1 to 9 */
+  buf[len++] = (char) (choose(9) + 1 + 48); // avoid 0-start number, generate number from 1-9
+  // buf[len++] = (char) (choose(10) + 48); 
+  buf[len++] = 'U'; // add U appendix to ensure unsigned operation
+}
+
+static void gen(char c) {
+  buf[len++] = c;
+}
+
+static void gen_rand_op() {
+  switch (choose(4)) {
+  case 0:
+    buf[len++] = '+';
+    break;
+  case 1:
+    buf[len++] = '-';
+    break;
+  case 2:
+    buf[len++] = '*';
+    break;
+  default:
+    buf[len++] = '/';
+    break;
+  }
+}
+
 static void gen_rand_expr() {
-  buf[0] = '\0';
+  switch (choose(4)) {
+    case 0: gen_num(); break;
+    case 1: gen('('); gen_rand_expr(); gen(')'); break;
+    case 2: gen_rand_expr(); gen_rand_op(); gen_rand_expr(); break;
+    default: gen(' '); gen_rand_expr(); gen(' '); break; // randomly generate space
+  }
 }
 
 int main(int argc, char *argv[]) {
   int seed = time(0);
   srand(seed);
-  int loop = 1;
+  int loop = 100;
   if (argc > 1) {
     sscanf(argv[1], "%d", &loop);
   }
   int i;
   for (i = 0; i < loop; i ++) {
+    /* reset the buf and codebuf */
+    memset(buf, 0, sizeof(buf));
+    memset(code_buf, 0, sizeof(code_buf));
+    len = 0;
+
     gen_rand_expr();
+    buf[len] = '\0';
+    printf("Expr generation round %d done, %s was generated\n", i+1, buf);
 
     sprintf(code_buf, code_format, buf);
 
@@ -53,17 +101,25 @@ int main(int argc, char *argv[]) {
     fputs(code_buf, fp);
     fclose(fp);
 
-    int ret = system("gcc /tmp/.code.c -o /tmp/.expr");
+    int ret = system("gcc -Werror /tmp/.code.c -o /tmp/.expr");
     if (ret != 0) continue;
 
-    fp = popen("/tmp/.expr", "r");
+    fp = popen("/tmp/.expr 2>/dev/null", "r");
     assert(fp != NULL);
 
     int result;
     ret = fscanf(fp, "%d", &result);
-    pclose(fp);
 
-    printf("%u %s\n", result, buf);
+    // get child thread exit status
+    int status = pclose(fp);
+
+    // check for division by zero (SIGFPE)
+    if (WIFSIGNALED(status) && WTERMSIG(status) == SIGFPE) {
+      /* do nothing */
+      continue;
+    } else {
+      printf("%u %s\n", result, buf);
+    }
   }
   return 0;
 }
