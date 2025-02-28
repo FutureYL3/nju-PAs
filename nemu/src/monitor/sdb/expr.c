c7d94ed213bfe7c8fab0e7439771b9aed974deb5
tracer-ics2022 <tracer@njuics.org>
2025-02-28 20:32:37 +0800

    >  compile NEMU 211221234 李四 Linux nju-PA 6.8.0-50-generic #51~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 21 12:03:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:32:37 up 9 days, 15:27,  7 users,  load average: 0.23, 0.11, 0.10

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -21,12 +21,14 @@
 #include <regex.h>
 /* for debug macro usage */
 #include <debug.h>
+/* for memory read */
+#include <memory/vaddr.h>
 
 enum {
   TK_NOTYPE = 256, TK_EQ,
 
   /* TODO: Add more token types */
-  TK_DEC, TK_LPAR, TK_RPAR
+  TK_DEC, TK_LPAR, TK_RPAR, TK_HEX, TK_REG, TK_NOTEQ, TK_AND, TK_DEREF
 };
 
 static struct rule {
@@ -39,6 +41,8 @@ static struct rule {
    */
 
   {" +", TK_NOTYPE},    // spaces
+	{"0x[0-9a-fA-F]+", TK_HEX}, // hexadecimal number
+	{"\\$(\\$0|ra|sp|gp|tp|t[0-6]|s[0-9]|a[0-7]|s10|s11)", TK_REG}, // register
 	{"[0-9]+", TK_DEC},     // decimal number
 	{"\\(", TK_LPAR},     // left parentheses
 	{"\\)", TK_RPAR},     // right parentheses
@@ -47,6 +51,8 @@ static struct rule {
   {"\\+", '+'},         // plus
 	{"-", '-'},           // minus
   {"==", TK_EQ},        // equal
+	{"!=", TK_NOTEQ},     // not equal
+	{"&&", TK_AND},       // and
 };
 
 #define NR_REGEX ARRLEN(rules)
@@ -109,6 +115,31 @@ static bool make_token(char *e) {
         switch (rules[i].token_type) {
 					case TK_NOTYPE:
 						break;
+					case TK_HEX:
+						// if out of buffer, panic and ends the debugger
+						if (substr_len > 31 + 2) { // "0x" is not counted
+							char *hex = (char *)calloc(substr_len, sizeof(char));
+							for (int j = 0; j < substr_len; ++ j) {
+								hex[j] = *(substr_start + j);
+							}
+							/* TODO */
+							panic("hex %s is larger than 32 bits\n", hex);
+						}
+
+						tokens[nr_token].type = TK_HEX;
+						for (int j = 2; j < substr_len; ++ j) { // ignore "0x"
+							tokens[nr_token].str[j-2] = *(substr_start + j);
+						}
+            tokens[nr_token].str[substr_len] = '\0';
+						nr_token++;
+						break;	
+					case TK_REG:
+						tokens[nr_token].type = TK_REG;
+						for (int j = 1; j < substr_len; ++ j) { // ignore "$"
+							tokens[nr_token].str[j-1] = *(substr_start + j);
+						}
+						nr_token++;
+						break;
 					case TK_DEC:
 						// if out of buffer, panic and ends the debugger
 						if (substr_len > 31) {
@@ -148,6 +179,12 @@ static bool make_token(char *e) {
 					case TK_EQ:
 						tokens[nr_token++].type = TK_EQ;
 						break;
+					case TK_NOTEQ:
+						tokens[nr_token++].type = TK_NOTEQ;
+						break;
+					case TK_AND:
+						tokens[nr_token++].type = TK_AND;
+						break;	
 					default: panic("unknown token type: %d\n", rules[i].token_type);
         }
 
@@ -183,6 +220,13 @@ word_t expr(char *e, bool *success) {
     return 0;
   }
 
+	/* check for dereference */
+	for (int i = 0; i < nr_token; ++ i) {
+		if (tokens[i].type == '*' && (i == 0 || tokens[i-1].type == TK_LPAR || tokens[i-1].type == '*' || tokens[i-1].type == '/' || tokens[i-1].type == '+' || tokens[i-1].type == '-' || tokens[i-1].type == TK_EQ || tokens[i-1].type == TK_NOTEQ || tokens[i-1].type == TK_AND)) {
+			tokens[i].type = TK_DEREF;
+		}
+	}
+	
   /* TODO: Insert codes to evaluate the expression. */
 	bool eval_success;
   word_t val = eval(0, nr_token - 1, &eval_success);
@@ -208,27 +252,71 @@ static word_t eval(int p, int q, bool *success) {
 		return 0;
  	}
   else if (p == q) {
+		// TODO: add hex and register eval
     /* Single token.
-		 * For now this token should be a number.
-		 * Return the value of the number.
+		 * For now this token should be a decimal/hexadecimal/register.
+		 * Return the value of the token.
 		 */
-		if (tokens[p].type != TK_DEC) {
+		if (tokens[p].type != TK_DEC && tokens[p].type != TK_HEX && tokens[p].type != TK_REG) {
 			*success = false;
-			fprintf(stderr, "token[%d] is not a number\n", p);
+			fprintf(stderr, "token[%d] is not a decimal/hexadecimal/register\n", p);
 			return 0;
 		}
 
 		word_t val = 0;
 		int mul = 1;
-	  int nr_str = 0;
-	  while (tokens[p].str[nr_str] != 0)  nr_str++;
-    // printf("nr_str of %s is %d\n", tokens[p].str, nr_str);
+		int nr_str = 0;
 
-		for (int i = nr_str - 1; i >= 0; -- i) {
-			val += (tokens[p].str[i] - 48) * mul;
-			mul *= 10;
+		switch (tokens[p].type) {
+			case TK_DEC:
+	  		while (tokens[p].str[nr_str] != 0)  nr_str++;
+    		// printf("nr_str of %s is %d\n", tokens[p].str, nr_str);
+
+				for (int i = nr_str - 1; i >= 0; -- i) {
+					word_t pre_val = val;
+					val += (tokens[p].str[i] - 48) * mul;
+					if ((tokens[p].str[i] - 48) * mul > val || pre_val > val) {
+						printf("Warning: token[%d] overflowed\n", p);
+					}
+					mul *= 10;
+				}
+				break;	
+			case TK_REG:
+				bool reg_success = false;
+				val = isa_reg_str2val(tokens[p].str, &reg_success);
+				if (!reg_success) {
+					*success = false;
+					fprintf(stderr, "evaluate the value of register %s at token[%d] failed\n", tokens[p].str, p);
+					return 0;
+				}
+				break;
+			case TK_HEX:
+				while (tokens[p].str[nr_str] != 0)  nr_str++;
+
+				for (int i = nr_str - 1; i >= 0; -- i) {
+					word_t pre_val = val;
+					int another_mul = 0;
+					switch (tokens[p].str[i]) {
+						case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9':
+							val += (tokens[p].str[i] - 48) * mul;
+							another_mul = tokens[p].str[i] - 48;
+							break;
+						case 'a': case 'b': case 'c': case 'd': case 'e': case 'f':
+							val += (tokens[p].str[i] - 87) * mul;
+							another_mul = tokens[p].str[i] - 87;
+							break;
+						case 'A': case 'B': case 'C': case 'D': case 'E': case 'F':
+							val += (tokens[p].str[i] - 55) * mul;
+							another_mul = tokens[p].str[i] - 55;
+							break;
+					}	
+					if (another_mul * mul > val || pre_val > val) {
+						printf("Warning: token[%d] overflowed\n", p);
+					}
+					mul *= 16;
+				}
+				break;
 		}
-		
 		// printf("hit number, index is %d, value is %u\n", p, val);
 		*success = true;
 		return val;		
@@ -240,45 +328,74 @@ static word_t eval(int p, int q, bool *success) {
 	  return eval(p + 1, q - 1, success);
  	}
   else {
+		// TODO: add == != && and *, treat * as special
 		if (!can_split) {
 			fprintf(stderr, "invalid can't split expr from token %d to %d\n", p, q);
 			*success = false;
 			return 0;
 		}
 
-    int op = find_main_op_position(p, q);
-		bool success1, success2;
-    word_t val1 = eval(p, op - 1, &success1);
-    word_t val2 = eval(op + 1, q, &success2);
+		if (tokens[p].type == TK_DEREF) {
+			bool eval_success = false;
+			word_t tmp = eval(p+1, q, &eval_success);
 
-		if (!success1 || !success2) {
-			*success = false;
-			return 0;
+			if (!eval_success) {
+				fprintf(stderr, "failed to evaluate expr from token %d to %d\n", p+1, q);
+				*success = false;
+				return 0;
+			}
+
+			vaddr_t addr = (vaddr_t) tmp;
+			word_t val = vaddr_read(addr, 4);
+
+			*success = true;
+			return val;
 		}
+		else {
+    	int op = find_main_op_position(p, q);
+			bool success1, success2;
+    	word_t val1 = eval(p, op - 1, &success1);
+    	word_t val2 = eval(op + 1, q, &success2);
 
-		*success = true;
-    switch (tokens[op].type) {
-      case '+': *success = true; return val1 + val2;
-      case '-': *success = true; return val1 - val2; 
-      case '*': *success = true; return val1 * val2; 
-      case '/': *success = true; return val1 / val2; 
-      default: *success = false; assert(0);
-    }
+			if (!success1 || !success2) {
+				*success = false;
+				return 0;
+			}
+
+			*success = true;
+    	switch (tokens[op].type) {
+      	case '+': *success = true; return val1 + val2;
+      	case '-': *success = true; return val1 - val2; 
+      	case '*': *success = true; return val1 * val2; 
+      	case '/': *success = true; return val1 / val2; 
+				case TK_EQ: *success = true; return val1 == val2 ? 1 : 0;
+				case TK_NOTEQ: *success = true; return val1 != val2 ? 1 : 0;
+				case TK_AND: *success = true; return (val1 > 0 && val2 > 0) ? 1 : 0;
+      	default: *success = false; assert(0);
+   		}
+		}
 	}		
 }
 
 static int get_precedence_level(int i) {
+	/* larger the number, more precident the level */
+	// TODO: consider when add == != and &&
 	switch (tokens[i].type) {
-		case '+': case '-':
+		case TK_AND:
+			return 0;
+		case TK_EQ: case TK_NOTEQ:
 			return 1;
-		case '*': case '/':
+		case '+': case '-':
 			return 2;
+		case '*': case '/':
+			return 3;
 	}
 
 	return 0;
 }
 
 static int find_main_op_position(int p, int q) {
+	// TODO: consider when add == != and &&
 	int op = -1, nr_lpar = 0;
 	for (int i = p; i <= q; ++ i) {
 		switch (tokens[i].type) {
@@ -288,10 +405,7 @@ static int find_main_op_position(int p, int q) {
 			case TK_RPAR:
 				nr_lpar--;
 				break;
-			case '+':
-			case '-':
-			case '*':
-			case '/':
+			case '+': case '-': case '*': case '/': case TK_EQ: case TK_NOTEQ: case TK_AND:
 				if (nr_lpar > 0)  continue;
 				if (op == -1) {
 					op = i;
