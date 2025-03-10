2d14298c5c0f9d8619023262febe41f285677e42
tracer-ics2022 <tracer@njuics.org>
2025-03-10 23:23:17 +0800

    >  compile NEMU 211221234 李四 Linux nju-PA 6.8.0-50-generic #51~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 21 12:03:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:23:17 up 15 days, 16:49,  9 users,  load average: 0.99, 0.64, 0.29

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -14,7 +14,61 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+	va_list ap;
+	va_start(ap, fmt);
+	int len = 0;
+	while (*fmt) {
+		if (*fmt == '%') {
+			++fmt;
+			switch (*fmt) {
+				case 's':
+					char *str = va_arg(ap, char *);
+					while (*str) {
+						out[len++] = *str;
+						++str;
+					}
+					break;
+				case 'd':
+					int num = va_arg(ap, int), i = 0, is_neg = 0;
+
+					if (num == 0) {
+						out[len++] = '0';
+						break;
+					}
+
+					if (num < 0) {
+						is_neg = 1;
+						num = -num;
+					}
+
+					while (num) {
+						out[len + i++] = num % 10 + '0';
+						num /= 10;
+					}
+					if (is_neg) {
+						out[len + i++] = '-';
+					}
+					int start = 0, end = i - 1;
+					while (start < end) {
+						char temp = out[len + start];
+					 	out[len + start] = out[len + end];
+						out[len + end] = temp;
+						++start; --end;
+					}
+					len += i;
+					break;
+				default:
+					halt(1);
+			}
+		}
+		else {
+			out[len++] = *fmt;
+		}
+		++fmt;
+	}
+	out[len] = '\0';
+	va_end(ap);
+	return len;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
