1de6fcd82cad79ddea777014b2cb8641d28b147f
tracer-ics2022 <tracer@njuics.org>
2025-03-13 23:56:43 +0800

    >  compile NEMU 211221234 李四 Linux nju-PA 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:56:43 up 11:10,  3 users,  load average: 0.07, 0.36, 0.22

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -5,8 +5,8 @@ void __am_timer_init() {
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-	uint32_t lower = inl(RTC_ADDR);
-	uint32_t upper = inl(RTC_ADDR + 4);
+	uint32_t upper = vaddr_read(RTC_ADDR + 4, 4);
+	uint32_t lower = vaddr_read(RTC_ADDR, 4);
 	uptime->us = 0;
 	uptime->us += upper;
 	uptime->us <<= 32;
