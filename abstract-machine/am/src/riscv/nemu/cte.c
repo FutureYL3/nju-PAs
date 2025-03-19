dcbe342112c8f1322fd81162cb26f440f64e4cff
tracer-ics2022 <tracer@njuics.org>
2025-03-19 22:34:35 +0800

    >  compile NEMU 211221234 李四 Linux nju-PA 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:34:35 up 2 days, 23:37,  9 users,  load average: 0.31, 0.30, 0.21

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -5,15 +5,6 @@
 static Context* (*user_handler)(Event, Context*) = NULL;
 
 Context* __am_irq_handle(Context *c) {
-  // for (int i = 0; i < 32; ++ i) {
-  //   printf("R[%d] is %x\n", i, (c->gpr[i]));
-  // }
-
-  // printf("mcause is %x\n", c->mcause);
-  // printf("mstatus is %x\n", c->mstatus);
-  // printf("mepc is %x\n", c->mepc);
-
-
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
@@ -21,7 +12,7 @@ Context* __am_irq_handle(Context *c) {
         ev.event = EVENT_YIELD;
         break;
       }
-      case 1: {
+      case 1: { // syscall
         ev.event = EVENT_SYSCALL;
         break;
       }
