4c8d7563098f781cd944cfa658347538b3d210f6
tracer-ics2022 <tracer@njuics.org>
2025-03-20 22:04:05 +0800

    >  compile NEMU 211221234 李四 Linux nju-PA 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:04:05 up 3 days, 17:16,  4 users,  load average: 0.11, 0.10, 0.14

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -9,16 +9,51 @@ void do_syscall(Context *c) {
   a[2] = c->GPR3;
   a[3] = c->GPR4;
 
+  char *name = NULL;
+
   switch (a[0]) {
     case SYS_yield: {
       yield();
       c->GPRx = 0;
+      name = "SYS_yield";
       break;
     }
     case SYS_exit: {
       halt(a[1]);
+      name = "SYS_exit";
+      break;
+    }
+    case SYS_write: {
+      c->GPRx = write(a[1], (void *) a[2], a[3]);
+      name = "SYS_write";
       break;
     }
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
+
+#if STRACE
+  if (name)  Log("syscall %s, with params a0=%d, a1=%d, a2=%d and ret=%d\n", name, a[1], a[2], a[3], c->GPRx);
+#endif
+}
+
+ssize_t write(int fd, const void *buf, size_t count) {
+  ssize_t written = 0;
+  switch (fd) {
+    /* for stdout and stderr */
+    case 1: case 2: {
+      char *p = (char *) buf;
+      for (int i = 0; i < count; ++ i)  {
+        putch(p[i]);
+        ++written;
+      }
+      break;
+    }
+  
+    default: {
+      Log("error occur in syscall write");
+      return -1;
+    }
+  }
+
+  return written;
 }
--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -63,8 +63,7 @@ int _open(const char *path, int flags, mode_t mode) {
 }
 
 int _write(int fd, void *buf, size_t count) {
-  _exit(SYS_write);
-  return 0;
+  return write(fd, buf, count);
 }
 
 void *_sbrk(intptr_t increment) {
