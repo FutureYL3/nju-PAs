130d00688868ecfdc4bae17335354fc2102b16b4
tracer-ics2022 <tracer@njuics.org>
2025-03-21 18:40:42 +0800

    >  compile NEMU 211221234 李四 Linux nju-PA 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:40:42 up 4 days,  3:07,  4 users,  load average: 0.48, 0.34, 0.27

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -9,29 +9,32 @@ void do_syscall(Context *c) {
   a[2] = c->GPR3;
   a[3] = c->GPR4;
 
-  char *name = NULL;
-
   switch (a[0]) {
     case SYS_yield: {
       yield();
       c->GPRx = 0;
-      name = "SYS_yield";
       break;
     }
     case SYS_exit: {
       halt(a[1]);
-      name = "SYS_exit";
       break;
     }
     case SYS_write: {
       c->GPRx = write(a[1], (void *) a[2], a[3]);
-      name = "SYS_write";
       break;
     }
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
 #if STRACE
+  char *name = NULL;
+
+  switch (a[0]) {
+    case SYS_yield: { name = "SYS_yield"; break; }
+    case SYS_exit: { name = "SYS_exit"; break; }
+    case SYS_write: { name = "SYS_write"; break; }
+  }
+
   if (name)  Log("syscall %s, with params a0=%d, a1=%d, a2=%d and ret=%d\n", name, a[1], a[2], a[3], c->GPRx);
 #endif
 }
