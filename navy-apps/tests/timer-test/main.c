21e3dadb4a6d83bd21e1f7c29a0d92f10fe88433
tracer-ics2022 <tracer@njuics.org>
2025-03-24 21:22:51 +0800

    >  compile NEMU 211221234 李四 Linux nju-PA 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:22:51 up 2 days,  1:06,  3 users,  load average: 0.50, 0.28, 0.21

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -1,47 +1,51 @@
 #include <stdio.h>
 #include <sys/time.h>
+#include <NDL.h>
 
 int main() {
-  long cur_sec = 0;
-  long cur_usec = 0;
-  struct timeval tv = {};
-  int ret = gettimeofday(&tv, NULL);
-  if (ret < 0) {
-    printf("Failed to get sys time at init time\n");
+  NDL_Init(0);
+  uint32_t cur_time = NDL_GetTicks();
+  if (time < 0) {
+    printf("Failed to get NDL sys time at init time\n");
     return -1;
   }
-  cur_sec = tv.tv_sec;
-  cur_usec = tv.tv_usec;
   int count = 1;
+  uint32_t time;
   // printf("start time: %ld.%ld\n", cur_sec, cur_usec);
   while (1) {
-    ret = gettimeofday(&tv, NULL);
-    if (ret < 0) {
+    time = NDL_GetTicks();
+    if (time < 0) {
       printf("Failed to get systime at %d times\n", count);
     }
+    // 500ms = 0.5s
+    if (time - cur_time < 500)  continue;
+
+    printf("already pass %d 0.5s\n", count++);
+    cur_time = time;
     // printf("current time: %ld.%ld\n", tv.tv_sec, tv.tv_usec);
-    if (tv.tv_sec == cur_sec) {
-      /* tolerate 100000 us(0.1s) diff */
-      if (tv.tv_usec - cur_usec > 450000 && tv.tv_usec - cur_usec < 550000) {
-        printf("already pass %d 0.5s\n", count++);
-        // ++count;
-      }
-      else continue;
-    }
-    else if (tv.tv_sec - cur_sec == 1) {
-      long us = tv.tv_usec + 1000000;
-      /* tolerate 100000 us(0.1s) diff */
-      if (us - cur_usec > 450000 && us - cur_usec < 550000) {
-        printf("already pass %d 0.5s\n", count++);
-        // ++count;
-      }
-      else continue;
-    }
+    // if (tv.tv_sec == cur_sec) {
+    //   /* tolerate 100000 us(0.1s) diff */
+    //   if (tv.tv_usec - cur_usec > 450000 && tv.tv_usec - cur_usec < 550000) {
+    //     printf("already pass %d 0.5s\n", count++);
+    //     // ++count;
+    //   }
+    //   else continue;
+    // }
+    // else if (tv.tv_sec - cur_sec == 1) {
+    //   long us = tv.tv_usec + 1000000;
+    //   /* tolerate 100000 us(0.1s) diff */
+    //   if (us - cur_usec > 450000 && us - cur_usec < 550000) {
+    //     printf("already pass %d 0.5s\n", count++);
+    //     // ++count;
+    //   }
+    //   else continue;
+
+    // }
 
     // if (count % 2 == 0)  printf("already pass %ds\n", count / 2);
 
-    cur_sec = tv.tv_sec;
-    cur_usec = tv.tv_usec;
+    // cur_sec = tv.tv_sec;
+    // cur_usec = tv.tv_usec;
   }
 
 }
