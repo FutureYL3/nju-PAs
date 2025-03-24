76fa4a990154db2f703b43e5a281fcf059c90088
tracer-ics2022 <tracer@njuics.org>
2025-03-24 17:32:32 +0800

    >  compile NEMU 211221234 李四 Linux nju-PA 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:32:32 up 1 day, 21:16,  3 users,  load average: 0.72, 0.81, 0.66

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -13,19 +13,19 @@ int main() {
   cur_sec = tv.tv_sec;
   cur_usec = tv.tv_usec;
   int count = 1;
-  printf("start time: %ld.%ld\n", cur_sec, cur_usec);
+  // printf("start time: %ld.%ld\n", cur_sec, cur_usec);
   while (1) {
     ret = gettimeofday(&tv, NULL);
     if (ret < 0) {
       printf("Failed to get systime at %d times\n", count);
     }
-    printf("current time: %ld.%ld\n", tv.tv_sec, tv.tv_usec);
+    // printf("current time: %ld.%ld\n", tv.tv_sec, tv.tv_usec);
     if (tv.tv_sec == cur_sec) {
       /* tolerate 100000 us(0.1s) diff */
       if (tv.tv_usec - cur_usec > 450000 && tv.tv_usec - cur_usec < 550000) {
         printf("already pass %d 0.5s\n", count++);
-        
       }
+      else continue;
     }
     else if (tv.tv_sec - cur_sec == 1) {
       long us = tv.tv_usec + 1000000;
@@ -33,6 +33,7 @@ int main() {
       if (us - cur_usec > 450000 && us - cur_usec < 550000) {
         printf("already pass %d 0.5s\n", count++);
       }
+      else continue;
     }
 
     cur_sec = tv.tv_sec;
