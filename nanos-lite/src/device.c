1fb3126334d4847fc6af8c4682d7bb2bd5ab404e
tracer-ics2022 <tracer@njuics.org>
2025-03-24 21:28:38 +0800

    >  compile NEMU 211221234 李四 Linux nju-PA 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:28:38 up 2 days,  1:12,  3 users,  load average: 0.71, 0.41, 0.29

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -15,8 +15,9 @@ static const char *keyname[256] __attribute__((used)) = {
 };
 
 size_t serial_write(const void *buf, size_t offset, size_t len) {
+  char *p = (char *) buf;
   for (int i = 0; i < len; ++ i) {
-    putch(((char *) buf)[i]);
+    putch(p[i]);
   }
   return len;
 }
