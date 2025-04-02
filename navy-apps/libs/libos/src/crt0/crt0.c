5b7c3ee0d1a63ff319a2e4e19219aa23d09c31a8
tracer-ics2022 <tracer@njuics.org>
2025-04-02 16:28:18 +0800

    >  compile NEMU 211221234 李四 Linux nju-PA 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:28:18 up 27 min,  1 user,  load average: 0.19, 0.11, 0.09

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -2,11 +2,15 @@
 #include <stdlib.h>
 #include <assert.h>
 
+void __libc_init_array(void);
+
 int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
 void call_main(uintptr_t *args) {
   char *empty[] =  {NULL };
   environ = empty;
+  /* call __libc_init_array to initialize cpp global object */
+  __libc_init_array();
   exit(main(0, empty, empty));
   assert(0);
 }
