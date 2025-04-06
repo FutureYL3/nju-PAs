abfd76706fe47f85c4f8e5497bdd0a2e9a10062c
tracer-ics2022 <tracer@njuics.org>
2025-04-06 20:47:13 +0800

    >  compile NEMU 211221234 李四 Linux nju-PA 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:47:13 up 1 day, 13:33,  1 user,  load average: 1.90, 1.56, 0.92

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -8,9 +8,9 @@ void __libc_init_array(void);
 int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
 void call_main(uintptr_t *args) {
-  unsigned int sp;
-  asm ("mv %0, sp" : "=r" (sp));
-  printf("current sp: 0x%x\n", sp);
+  // unsigned int sp;
+  // asm ("mv %0, sp" : "=r" (sp));
+  // printf("current sp: 0x%x\n", sp);
   // uintptr_t *p = (uintptr_t *) args;
   int argc = *args;
   char **argv = (char **) (++args);
