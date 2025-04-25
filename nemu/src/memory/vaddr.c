b7b53935b1d981ed0de9d77322cc8ac3923d9955
tracer-ics2022 <tracer@njuics.org>
2025-04-25 23:03:54 +0800

    >  compile NEMU 211221234 李四 Linux nju-PA 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:03:54 up  7:55,  1 user,  load average: 0.20, 0.18, 0.33

--- nemu/src/memory/vaddr.c
+++ nemu/src/memory/vaddr.c
@@ -21,9 +21,11 @@ word_t vaddr_ifetch(vaddr_t addr, int len) {
   switch (isa_mmu_check(addr, len, MEM_TYPE_IFETCH)) {
     case MMU_DIRECT: {
       ret = paddr_read(addr, len);
+      break;
     }
     case MMU_TRANSLATE: {
       ret = paddr_read(isa_mmu_translate(addr, len, MEM_TYPE_IFETCH), len);
+      break;
     }
     case MMU_FAIL: {
       panic("MMU_FAIL");
@@ -38,9 +40,11 @@ word_t vaddr_read(vaddr_t addr, int len) {
   switch (isa_mmu_check(addr, len, MEM_TYPE_READ)) {
     case MMU_DIRECT: {
       ret = paddr_read(addr, len);
+      break;
     }
     case MMU_TRANSLATE: {
       ret = paddr_read(isa_mmu_translate(addr, len, MEM_TYPE_READ), len);
+      break;
     }
     case MMU_FAIL: {
       panic("MMU_FAIL");
@@ -54,9 +58,11 @@ void vaddr_write(vaddr_t addr, int len, word_t data) {
   switch (isa_mmu_check(addr, len, MEM_TYPE_WRITE)) {
     case MMU_DIRECT: {
       paddr_write(addr, len, data);
+      break;
     }
     case MMU_TRANSLATE: {
       paddr_write(isa_mmu_translate(addr, len, MEM_TYPE_WRITE), len, data);
+      break;
     }
     case MMU_FAIL: {
       panic("MMU_FAIL");
