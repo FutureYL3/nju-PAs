cb39f33825dc3b5abeef6bf5e414dcb039eff740
tracer-ics2022 <tracer@njuics.org>
2025-04-26 00:30:03 +0800

    >  compile NEMU 211221234 李四 Linux nju-PA 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:30:03 up  9:21,  1 user,  load average: 2.62, 1.31, 0.67

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -33,9 +33,10 @@ bool vme_init(void* (*pgalloc_f)(int), void (*pgfree_f)(void*)) {
   int i;
   for (i = 0; i < LENGTH(segments); i ++) {
     void *va = segments[i].start;
-    printf("0x%p\n", (char *) va);
+    
     for (; va < segments[i].end; va += PGSIZE) {
       map(&kas, va, va, 0);
+      printf("0x%p\n", (char *) va);
     }
   }
 
